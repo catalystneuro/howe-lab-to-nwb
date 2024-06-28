dcfa7aa24e94a486a9c2c63159e43e8d9d57e2cf
weiglszonja <wszonja@gmail.com>
2024-06-28 13:47:08 +0200

    add all velocities as timeseries

--- src/howe_lab_to_nwb/vu2024/interfaces/vu2024_behaviorinterface.py
+++ src/howe_lab_to_nwb/vu2024/interfaces/vu2024_behaviorinterface.py
@@ -4,10 +4,9 @@ import numpy as np
 import pandas as pd
 from neuroconv import BaseTemporalAlignmentInterface
 from neuroconv.tools import get_module
-from neuroconv.utils import FilePathType, get_base_schema
+from neuroconv.utils import FilePathType, get_base_schema, get_schema_from_hdmf_class
 from pymatreader import read_mat
 from pynwb import NWBFile, TimeSeries
-from pynwb.behavior import CompassDirection
 from pynwb.epoch import TimeIntervals
 
 
@@ -60,32 +59,14 @@ class Vu2024BehaviorInterface(BaseTemporalAlignmentInterface):
     def get_metadata_schema(self) -> dict:
         metadata_schema = super().get_metadata_schema()
         metadata_schema["properties"]["Behavior"] = get_base_schema(tag="Behavior")
+        time_series_schema = get_schema_from_hdmf_class(TimeSeries)
         metadata_schema["properties"]["Behavior"].update(
-            required=["CompassDirection", "TimeSeries", "TimeIntervals"],
+            required=["TimeSeries", "TimeIntervals"],
             properties=dict(
-                CompassDirection=dict(
-                    type="object",
-                    required=["SpatialSeries"],
-                    properties=dict(
-                        SpatialSeries=dict(
-                            type="object",
-                            required=["name", "description", "unit"],
-                            properties=dict(
-                                name=dict(type="string"),
-                                description=dict(type="string"),
-                                unit=dict(type="string"),
-                            ),
-                        ),
-                    ),
-                ),
                 TimeSeries=dict(
-                    type="object",
-                    required=["name", "description", "unit"],
-                    properties=dict(
-                        name=dict(type="string"),
-                        description=dict(type="string"),
-                        unit=dict(type="string"),
-                    ),
+                    type="array",
+                    minItems=1,
+                    items=time_series_schema,
                 ),
                 TimeIntervals=dict(
                     type="object",
@@ -103,19 +84,18 @@ class Vu2024BehaviorInterface(BaseTemporalAlignmentInterface):
         metadata = super().get_metadata()
 
         metadata["Behavior"] = dict(
-            CompassDirection=dict(
-                SpatialSeries=dict(
-                    name="SpatialSeries",
-                    description="The yaw (rotational) velocity measured in degrees/s.",
-                    reference_frame="unknown",
-                    unit="degrees/s",
+            TimeSeries=[
+                dict(
+                    name="AngularVelocity",
+                    description="The angular velocity from yaw (rotational) velocity converted to radians/s.",
+                    unit="radians/s",
                 ),
-            ),
-            TimeSeries=dict(
-                name="Velocity",
-                description="Velocity for the roll and pitch (x, y) measured in m/s.",
-                unit="m/s",
-            ),
+                dict(
+                    name="Velocity",
+                    description="Velocity for the roll and pitch (x, y) measured in m/s.",
+                    unit="m/s",
+                ),
+            ],
             TimeIntervals=dict(
                 name="TimeIntervals",
                 description="The onset times of the events (licking, tone, light or reward delivery).",
@@ -124,10 +104,17 @@ class Vu2024BehaviorInterface(BaseTemporalAlignmentInterface):
 
         return metadata
 
-    def add_velocity_signals(self, nwbfile: NWBFile, metadata: dict, stub_test: Optional[bool] = False) -> None:
+    def add_velocity_signals(
+        self,
+        nwbfile: NWBFile,
+        metadata: dict,
+        ball_diameter_in_meters: float = None,
+        stub_test: Optional[bool] = False,
+    ) -> None:
         """
         Add the velocity signals (yaw (z) velocity and the roll and pitch (x, y)) to the NWBFile.
-        The yaw velocity is measured in degrees/s and the roll and pitch velocities are measured in m/s.
+        The yaw velocity is converted to radians/s using the calculation provided from the Howe Lab.
+        The roll and pitch velocities are in m/s.
 
         Parameters
         ----------
@@ -135,6 +122,10 @@ class Vu2024BehaviorInterface(BaseTemporalAlignmentInterface):
             The NWBFile to which the velocity signals will be added.
         metadata : dict
             Metadata for the velocity signals.
+        ball_diameter_in_meters : float, optional
+            The diameter of the ball in meters, optional.
+            Used to convert the yaw velocity from m/s to radians/s, when not specified default value is 0.2032.
+            Reference: https://zenodo.org/records/10272874 from ball2xy.m
         stub_test : bool, optional
             Whether to run a stub test, by default False.
         """
@@ -142,32 +133,38 @@ class Vu2024BehaviorInterface(BaseTemporalAlignmentInterface):
         end_frame = 6000 if stub_test else None
 
         behavior_metadata = metadata["Behavior"]
-        spatial_series_metadata = behavior_metadata["CompassDirection"]["SpatialSeries"]
+        time_series_metadata = behavior_metadata["TimeSeries"]
 
         behavior_data = read_mat(filename=self.source_data["file_path"])
         timestamps = self.get_timestamps(stub_test=stub_test)
-
-        compass_direction = CompassDirection(name="CompassDirection")
-
-        compass_direction.create_spatial_series(
-            **spatial_series_metadata,
-            data=behavior_data["ballYaw"][:end_frame],
-            timestamps=timestamps,
-        )
 
         behavior = get_module(
             nwbfile,
             name="behavior",
             description="Contains the velocity signals from two optical mouse sensors (Logitech G203 mice with hard plastic shells removed).",
         )
-        behavior.add(compass_direction)
 
-        velocity_metadata = behavior_metadata["TimeSeries"]
-        data = np.column_stack((behavior_data["ballRoll"], behavior_data["ballPitch"]))
+        # ballYaw is in m/s convert to radians/s using their code
+        # https://zenodo.org/records/10272874 ball2xy.m
+        angular_velocity_metadata = time_series_metadata[0]
+        yaw_in_meters = behavior_data["ballYaw"][:end_frame]
+        ball_diameter_in_meters = ball_diameter_in_meters or 0.2032
+        yaw_in_radians = yaw_in_meters / (np.pi * ball_diameter_in_meters) * 2 * np.pi
+
+        velocity_z = TimeSeries(
+            **angular_velocity_metadata,
+            data=yaw_in_radians,
+            timestamps=timestamps,
+        )
+        behavior.add(velocity_z)
+
+        velocity_metadata = time_series_metadata[1]
+        velocity_in_meters = np.column_stack((behavior_data["ballRoll"], behavior_data["ballPitch"]))
+        velocity_in_meters = velocity_in_meters[:end_frame]
         velocity_xy = TimeSeries(
             **velocity_metadata,
-            data=data[:end_frame],
-            timestamps=timestamps,
+            data=velocity_in_meters,
+            timestamps=velocity_z,
         )
         behavior.add(velocity_xy)
 
@@ -255,7 +252,13 @@ class Vu2024BehaviorInterface(BaseTemporalAlignmentInterface):
         nwbfile: NWBFile,
         metadata: dict,
         stub_test: Optional[bool] = False,
+        ball_diameter_in_meters: Optional[float] = None,
     ) -> None:
 
-        self.add_velocity_signals(nwbfile=nwbfile, metadata=metadata, stub_test=stub_test)
+        self.add_velocity_signals(
+            nwbfile=nwbfile,
+            metadata=metadata,
+            stub_test=stub_test,
+            ball_diameter_in_meters=ball_diameter_in_meters,
+        )
         self.add_binary_signals(nwbfile=nwbfile, metadata=metadata)

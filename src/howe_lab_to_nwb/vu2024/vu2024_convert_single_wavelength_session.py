ede551947397c7acdf5452b36b71a62de3da4ed2
weiglszonja <wszonja@gmail.com>
2024-07-17 10:49:07 +0200

    fix append mode backend configuration

--- src/howe_lab_to_nwb/vu2024/vu2024_convert_single_wavelength_session.py
+++ src/howe_lab_to_nwb/vu2024/vu2024_convert_single_wavelength_session.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from typing import Union, Optional, List
 
 from dateutil import tz
+from neuroconv.tools.nwb_helpers import configure_and_write_nwbfile
 from neuroconv.utils import load_dict_from_file, dict_deep_update
 from pynwb import NWBFile
 
@@ -209,13 +210,7 @@ def single_wavelength_session_to_nwb(
     if nwbfile_path is None:
         return nwbfile
 
-    converter.run_conversion(
-        nwbfile_path=nwbfile_path,
-        nwbfile=nwbfile,
-        metadata=metadata,
-        conversion_options=conversion_options,
-        overwrite=True,
-    )
+    configure_and_write_nwbfile(nwbfile=nwbfile, output_filepath=nwbfile_path)
 
 
 if __name__ == "__main__":
